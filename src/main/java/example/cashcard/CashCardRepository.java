5c4703515c4ff53da9ca718469d5006ddc4f8d49
Lahrach Omar <lahrach.omar.1999@gmail.com>
2024-02-17 13:07:43 +0100

    adding post request and tests

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,2 +1,6 @@
-package example.cashcard;public class CashCardRepository {
+package example.cashcard;
+
+import org.springframework.data.repository.CrudRepository;
+
+interface CashCardRepository extends CrudRepository<CashCard, Long> {
 }
